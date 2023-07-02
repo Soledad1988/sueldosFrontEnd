ddc5fab0cdeab8f600c2ee4de50e89eda6646fe8
Brenda <brentorres26@gmail.com>
2023-07-02 14:53:16 -0300

    componente Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componenets/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'menu', pathMatch:'full'},
+  {path:'menu', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
