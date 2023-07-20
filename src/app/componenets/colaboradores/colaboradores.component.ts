a3a01ca2c3eb062e7a7f7b4a3b08931291515e4c
Brenda <brentorres26@gmail.com>
2023-07-20 11:47:44 -0300

    corrección de componentes

--- src/app/componenets/colaboradores/colaboradores.component.ts
+++ src/app/componenets/colaboradores/colaboradores.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ColaboradorService } from 'src/app/service/colaborador.service';
 
 @Component({
   selector: 'app-colaboradores',
   templateUrl: './colaboradores.component.html',
   styleUrls: ['./colaboradores.component.css']
 })
-export class ColaboradoresComponent {
+export class ColaboradoresComponent implements OnInit{
+
+  lista:any=[];
+
+  constructor(private colaboradorService: ColaboradorService){}
+
+  ngOnInit(): void {
+    this.listar();
+  }
+
+  listar(): void
+  {
+    this.colaboradorService.colaborador().subscribe(
+      res=>{
+        this.lista=res;
+        console.log(res);
+      },
+      err=>console.log(err)
+    );
+
+  }
 
 }
