4ae6829cdc3cae0f9c131275f6eafedd6228c488
Brenda <brentorres26@gmail.com>
2023-07-20 11:48:25 -0300

    Ekiminación de un componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,28 @@ import { HomeComponent } from './componenets/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ColaboradoresComponent } from './componenets/colaboradores/colaboradores.component';
 
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+
 //externos
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { NuevoColaboradorComponent } from './componenets/formularios/nuevo-colaborador/nuevo-colaborador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ColaboradoresComponent
+    ColaboradoresComponent,
+    NuevoColaboradorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatListModule,
     MatButtonModule,
