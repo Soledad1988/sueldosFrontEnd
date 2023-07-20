22e009784673e2327b7d86bf3f2420c5c8aae44f
Brenda <brentorres26@gmail.com>
2023-07-20 11:46:11 -0300

    configuración de nueva ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componenets/home/home.component';
 import { ColaboradoresComponent } from './componenets/colaboradores/colaboradores.component';
+import { NuevoColaboradorComponent } from './componenets/formularios/nuevo-colaborador/nuevo-colaborador.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'menu', pathMatch:'full'},
   {path:'menu', component:HomeComponent},
-  {path:'colaborador', component:ColaboradoresComponent}
+  {path:'colaborador', component:ColaboradoresComponent},
+  {path:'nuevo', component:NuevoColaboradorComponent}
 ];
 
 @NgModule({
