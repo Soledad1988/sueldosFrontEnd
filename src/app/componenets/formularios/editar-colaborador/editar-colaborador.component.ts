3942a4787c0de3ea575bffaf94921aeeaa951106
Brenda <brentorres26@gmail.com>
2023-07-20 21:50:28 -0300

    creación componente formularios-editar

---
+++ src/app/componenets/formularios/editar-colaborador/editar-colaborador.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Colaborador } from 'src/app/models/colaborador';
+import { ColaboradorService } from 'src/app/service/colaborador.service';
+
+@Component({
+  selector: 'app-editar-colaborador',
+  templateUrl: './editar-colaborador.component.html',
+  styleUrls: ['./editar-colaborador.component.css']
+})
+export class EditarColaboradorComponent implements OnInit{
+
+  id=null;
+  actual:Colaborador=new Colaborador();
+  suscription: Subscription = new Subscription();
+
+  constructor(
+    private colaboradorService:ColaboradorService,
+    private antivatedRouter: ActivatedRoute,
+    private router:Router
+    ) { }
+
+    ngOnInit(): void {
+      this.getColaborador();
+
+      this.suscription = this.colaboradorService.refresh$.subscribe(()=>{
+        this.getColaborador();
+      })
+    }
+
+    
+    getColaborador():void{
+    this.antivatedRouter.params.subscribe(
+      e=>{
+        let id=e['id'];
+        if(id){
+          this.colaboradorService.UnColaborador(id).subscribe(
+            res=>this.actual=res
+          );
+        }
+      }
+    );
+  }
+
+  //metodo para implementar la logica del guardado
+  guardar():void{
+    console.log(this.actual);
+    this.colaboradorService.guardar(this.actual).subscribe(
+      res=>{
+
+      this.router.navigate(['/colaborador'])
+    });
+  } 
+}
--- src/app/componenets/formularios/nuevo-colaborador/nuevo-colaborador.component.ts
+++ src/app/componenets/formularios/nuevo-colaborador/nuevo-colaborador.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Colaborador } from 'src/app/models/colaborador';
+import { ColaboradorService } from 'src/app/service/colaborador.service';
 
 @Component({
   selector: 'app-nuevo-colaborador',
   templateUrl: './nuevo-colaborador.component.html',
   styleUrls: ['./nuevo-colaborador.component.css']
 })
-export class NuevoColaboradorComponent {
+export class NuevoColaboradorComponent implements OnInit{
+
+  nuevo: Colaborador = new Colaborador();
+
+  constructor(private colaboradorService:ColaboradorService, 
+    private router:Router, 
+    private activatedRoute:ActivatedRoute){}
+
+  ngOnInit(): void {
+  }
+
+  agregar(){
+    this.colaboradorService.guardar(this.nuevo).subscribe(
+      res=>{
+        console.log(res);
+        this.router.navigate(['/colaborador']);
+      },
+      err=>console.log(err)
+    );
+  }
 
 }
