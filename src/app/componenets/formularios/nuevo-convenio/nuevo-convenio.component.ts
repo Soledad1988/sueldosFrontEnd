f239594afc52d0b7680a2ebee26c8677a9ede3c0
Brenda <brentorres26@gmail.com>
2023-12-08 15:16:58 -0300

    Se creo nuevo componente

--- src/app/componenets/formularios/nuevo-convenio/nuevo-convenio.component.ts
+++ src/app/componenets/formularios/nuevo-convenio/nuevo-convenio.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Convenio } from 'src/app/models/Convenio';
 import { ConvenioService } from 'src/app/service/convenio.service';
+import Swal from 'sweetalert2';
+
+
+
 
 @Component({
   selector: 'app-nuevo-convenio',
@@ -9,6 +13,8 @@ import { ConvenioService } from 'src/app/service/convenio.service';
   styleUrls: ['./nuevo-convenio.component.css']
 })
 export class NuevoConvenioComponent implements OnInit{
+
+  title='Convenio Registardo';
 
   nuevo: Convenio = new Convenio();
 
@@ -21,10 +27,11 @@ export class NuevoConvenioComponent implements OnInit{
     
   }
 
-  agregar(){
+ agregar(){
     this.convenioService.guardar(this.nuevo).subscribe(
       res=>{
         console.log(res);
+        Swal.fire(this.title,`Convenio creado con exito`, 'success') //${res.idConvenio}
         this.router.navigate(['/convenio']);
       },
       err=>console.log(err)
