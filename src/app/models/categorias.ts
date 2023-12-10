27a1312d546cb4436854f89e76b31e3cee80cd22
Brenda <brentorres26@gmail.com>
2023-12-10 14:45:00 -0300

    modificacion clase categoria

--- src/app/models/categorias.ts
+++ src/app/models/categorias.ts
@@ -1,8 +1,10 @@
-import { Convenio } from "./Convenio";
+
 
 export class Categoria{
     idCategoria?:number;
     nombre?:String;
     descripcion?:String;
-    Convenio?:Convenio;
+    Convenio?:{
+        idConvenio?: number;
+      };
 }
