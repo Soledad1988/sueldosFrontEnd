14aac5306410b8b62857dec7237ec0a3893353ca
Brenda <brentorres26@gmail.com>
2023-12-10 14:43:39 -0300

    cambios en formularios nueva categoria

--- src/app/componenets/formularios/nueva-categoria/nueva-categoria.component.ts
+++ src/app/componenets/formularios/nueva-categoria/nueva-categoria.component.ts
@@ -4,6 +4,7 @@ import { CategoriaService } from 'src/app/service/categoria.service';
 import { Categoria } from 'src/app/models/categorias';
 import Swal from 'sweetalert2';
 import { HttpClient } from '@angular/common/http';
+import { ConvenioService } from 'src/app/service/convenio.service';
 
 @Component({
   selector: 'app-nueva-categoria',
@@ -13,25 +14,66 @@ import { HttpClient } from '@angular/common/http';
 export class NuevaCategoriaComponent implements OnInit{
 
   title='Categoria Registrada';
-
-  nuevaCategoria: Categoria = new Categoria();
+  
 
   constructor(private categoriaService:CategoriaService, 
+    private convenioService:ConvenioService,
     private router:Router, 
     private activatedRoute:ActivatedRoute,
     private http: HttpClient){}
 
-    con: any[] = [];
+    convenios: any[] = [];
+
+    //nuevaCategoria: Categoria = new Categoria();
+
+    nuevaCategoria = {
+      id_categoria:'',
+      nombre:'',
+      descripcion:'',
+      convenio:{
+        idConvenio:''
+      }
+    }
 
   ngOnInit(): void {
 
-    this.http.get<any[]>('http://localhost:8080/convenio').subscribe((data) => {
-      this.con = data;
-    });
-  
+    this.convenioService.convenio().subscribe(
+      (dato:any) => {
+        this.convenios = dato;
+        console.log(this.convenios);
+      },(error) => {
+        console.log(error);
+        Swal.fire('Error !!','Error al cargar los datos','error');
+      }
+    )
+
+  }
+
+  agregarCategoria(){
+    console.log(this.nuevaCategoria);
+
+    this.categoriaService.crearCategoria(this.nuevaCategoria).subscribe(
+      (data) => {
+        console.log(data);
+        Swal.fire('Examen guardado','El examen ha sido guardado con éxito','success');
+        this.nuevaCategoria = {
+            id_categoria:'',
+            nombre:'',
+            descripcion:'',
+            convenio:{
+              idConvenio:''
+            }
+        }
+        this.router.navigate(['/categoria']);
+      },
+      (error) => {
+        Swal.fire('Error','Error al guardar el examen','error');
+      }
+    )
   }
 
 
+/*
   agregarCategoria(){
     this.categoriaService.guardar(this.nuevaCategoria).subscribe(
       res=>{
@@ -39,8 +81,12 @@ export class NuevaCategoriaComponent implements OnInit{
         Swal.fire(this.title,`Categoria creada con exito`, 'success')
         this.router.navigate(['/categoria']);
       },
-      err=>console.log(err)
+      err => {
+        console.error('Error al guardar la categoría', err);
+        // Puedes manejar el error de manera adecuada, por ejemplo, mostrando un mensaje al usuario.
+      }
     );
-  }
+  }*/
+
 
 }
