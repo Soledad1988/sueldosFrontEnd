67e0fee8643ef1c4fd35dee38aa9d1c9319bea06
Brenda <brentorres26@gmail.com>
2023-12-11 16:49:14 -0300

    Html y servicio del template nuevo colaborador

--- src/app/componenets/formularios/nuevo-colaborador/nuevo-colaborador.component.ts
+++ src/app/componenets/formularios/nuevo-colaborador/nuevo-colaborador.component.ts
@@ -1,7 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Colaborador } from 'src/app/models/colaborador';
+import { CategoriaService } from 'src/app/service/categoria.service';
 import { ColaboradorService } from 'src/app/service/colaborador.service';
+import { ConvenioService } from 'src/app/service/convenio.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-nuevo-colaborador',
@@ -10,23 +13,87 @@ import { ColaboradorService } from 'src/app/service/colaborador.service';
 })
 export class NuevoColaboradorComponent implements OnInit{
 
-  nuevo: Colaborador = new Colaborador();
+  convenios: any[] = [];
 
-  constructor(private colaboradorService:ColaboradorService, 
-    private router:Router, 
-    private activatedRoute:ActivatedRoute){}
+  categorias: any[] = [];
+
+  nuevoColaborador = {
+    id:'',
+    nombre:'',
+    apellido:'',
+    dni:'',
+    nacimiento:'',
+    edad:'',
+    direccion:'',
+    activo:'false',
+    convenio:{
+      idConvenio:''
+    },
+    categoria:{
+      idCategoria:''
+    }
+  }
+
+
+  constructor(private colaboradorService: ColaboradorService,
+    private convenioService:ConvenioService,
+    private categoriaService:CategoriaService,
+    private router:Router){}
 
   ngOnInit(): void {
+
+    this.convenioService.convenio().subscribe(
+      (dato:any) => {
+        this.convenios = dato;
+        console.log(this.convenios);
+      },(error) => {
+        console.log(error);
+        Swal.fire('Error !!','Error al cargar los datos','error');
+      }
+    )
+
+    //++++++++++++++++++++++++++++
+
+    this.categoriaService.categoria().subscribe(
+      (dato:any) => {
+        this.categorias = dato;
+        console.log(this.categorias);
+      },(error) => {
+        console.log(error);
+        Swal.fire('Error !!','Error al cargar los datos','error');
+      }
+    )
   }
 
   agregar(){
-    this.colaboradorService.guardar(this.nuevo).subscribe(
-      res=>{
-        console.log(res);
+    console.log(this.nuevoColaborador);
+
+    this.colaboradorService.crearColaborador(this.nuevoColaborador).subscribe(
+      (data) => {
+        console.log(data);
+        Swal.fire('Colaborador guardado','El colaborador ha sido guardado con éxito','success');
+        this.nuevoColaborador = {
+          id:'',
+          nombre:'',
+          apellido:'',
+          dni:'',
+          nacimiento:'',
+          edad:'',
+          direccion:'',
+          activo:'false',
+          convenio:{
+            idConvenio:''
+          },
+          categoria:{
+            idCategoria:''
+          }
+        }
         this.router.navigate(['/colaborador']);
       },
-      err=>console.log(err)
-    );
+      (error) => {
+        Swal.fire('Error','Error al guardar el registro','error');
+      }
+    )
   }
 
 }
