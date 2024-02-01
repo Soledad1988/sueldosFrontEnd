e4904d672a5becf3742205d4a7906f748d51b7dd
Brenda <brentorres26@gmail.com>
2024-02-01 16:11:49 -0300

    Cambios en componente categorias

--- src/app/componenets/categorias/categorias.component.ts
+++ src/app/componenets/categorias/categorias.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CategoriaService } from 'src/app/service/categoria.service';
+import { ConvenioService } from 'src/app/service/convenio.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-categorias',
@@ -8,12 +11,39 @@ import { CategoriaService } from 'src/app/service/categoria.service';
 })
 export class CategoriasComponent implements OnInit{
 
+  title='Categoria Registrada';
+  
   lista:any=[];
 
-  constructor(private categoriasService:CategoriaService){}
+  convenios: any[] = [];
+
+  constructor(private categoriasService:CategoriaService,
+    private categoriaService:CategoriaService, 
+    private convenioService:ConvenioService,
+    private router:Router, ){}
   
+  nuevaCategoria = {
+    id_categoria:'',
+    nombre:'',
+    monto:'',
+    descripcion:'',
+    convenio:{
+      idConvenio:''
+    }
+  }
+
   ngOnInit(): void {
     this.listar();
+
+    this.convenioService.convenio().subscribe(
+      (dato:any) => {
+        this.convenios = dato;
+        console.log(this.convenios);
+      },(error) => {
+        console.log(error);
+        Swal.fire('Error !!','Error al cargar los datos','error');
+      }
+    )
   }
 
   listar(): void
@@ -35,5 +65,30 @@ export class CategoriasComponent implements OnInit{
       err=>console.log(err)
     );
   }
+
+  agregarCategoria(){
+    console.log(this.nuevaCategoria);
+
+    this.categoriaService.crearCategoria(this.nuevaCategoria).subscribe(
+      (data) => {
+        console.log(data);
+        Swal.fire('Categoria guardada','La categoria ha sido guardado con éxito','success');
+        this.nuevaCategoria = {
+            id_categoria:'',
+            nombre:'',
+            monto:'',
+            descripcion:'',
+            convenio:{
+              idConvenio:''
+            }
+        }
+        this.router.navigate(['/categoria']);
+      },
+      (error) => {
+        Swal.fire('Error','Error al guardar el examen','error');
+      }
+    )
+  }
+
   
 }
