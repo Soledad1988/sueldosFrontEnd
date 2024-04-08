0a5343258fe8c3e84dd1fe888188b133bb37c481
Brenda <brentorres26@gmail.com>
2024-04-08 18:55:45 -0300

    Cambios en service y models novedades

--- src/app/service/archivo.service.ts
+++ src/app/service/archivo.service.ts
@@ -16,4 +16,12 @@ export class ArchivoService {
   generarExcel() {
     return this.http.get('http://localhost:8080/generar/excel', { responseType: 'blob' });
   }
+
+  imprimirPdfNoedades(colaborador: Colaborador) {
+    return this.http.get('http://localhost:8080/novedad/pdf', { responseType: 'blob' });
+  }
+
+  generarExcelNovedades() {
+    return this.http.get('http://localhost:8080/novedad/excel', { responseType: 'blob' });
+  }
 }
